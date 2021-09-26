f3d028e6dddf06f75fb5841d5c9d906f819e26f7
thiagoffontes <thiagoffontes@icloud.com>
2021-09-26 17:37:55 -0300

    changed font to dejavu sans

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "xft:Dejavu Sans Mono:pixelsize=14" };
+static const char dmenufont[]       = "xft:Dejavu Sans Mono:pixelsize=14";
 
 
 static const char norm_fg[]         = "#311f3a";
