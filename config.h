d5658ba29a9ab17e5e6bdcf8d39049c14a8d2807
Thiago <thiagoffontes@icloud.com>
2021-09-30 20:38:09 -0300

    removed active indicator, tags set to 4

--- config.h
+++ config.h
@@ -31,7 +31,8 @@ static char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"1", "2", "3", "4"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -127,11 +128,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+//	tagkeys(                        XK_5,                      4)
+//	TAGKEYS(                        XK_6,                      5)
+//	TAGKEYS(                        XK_7,                      6)
+//	TAGKEYS(                        XK_8,                      7)
+//	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      		quit,           	{0} },
 };
 
