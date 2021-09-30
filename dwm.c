b832e20dc572d3d7f6b6bb64adcd0e9c80c29b77
Thiago <thiagoffontes@icloud.com>
2021-09-30 20:43:19 -0300

    removed indicator for floating

--- dwm.c
+++ dwm.c
@@ -752,8 +752,8 @@ drawbar(Monitor *m)
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-			if (m->sel->isfloating)
-				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+//			if (m->sel->isfloating)
+//				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
